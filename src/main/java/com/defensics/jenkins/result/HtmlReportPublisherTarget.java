613a873894e4e35c52e84d9cf276837f51e21736
Matti Varanka <mtv@blackduck.com>
2024-10-30 15:14:02 +0200

    Update copyrights

--- src/main/java/com/defensics/jenkins/result/HtmlReportPublisherTarget.java
+++ src/main/java/com/defensics/jenkins/result/HtmlReportPublisherTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020-2023 Synopsys, Inc.
+ * Copyright 2024 Black Duck Software, Inc. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
